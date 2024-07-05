b9d1623c4feb0ff93bc3037de1caeb2032cf9740
William Hleucka <william.hleucka@gmail.com>
2024-07-05 19:53:16 +0000

    fix: button size list actions

--- templates/module/index/index.php
+++ templates/module/index/index.php
@@ -3,10 +3,10 @@
 </section>
 <section id="actions" class="mb-3">
 	<?php if ($actions['show_create_action']) : ?>
-		<button type="button" hx-get="/admin/<?= $module ?>/create" hx-swap="outerHTML" hx-select="#content" hx-target="#content" class="btn btn-success">Create</button>
+		<button type="button" hx-get="/admin/<?= $module ?>/create" hx-swap="outerHTML" hx-select="#content" hx-target="#content" class="btn btn-success btn-sm">Create</button>
 	<?php endif ?>
 	<?php if ($actions['show_export_action']) : ?>
-	<a type="button" href="?export_csv" class="btn btn-success">Export</a>
+	<a type="button" href="?export_csv" class="btn btn-success btn-sm">Export</a>
 	<?php endif ?>
 </section>
 <section id="filters-table">
