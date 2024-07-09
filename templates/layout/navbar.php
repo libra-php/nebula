92699f7a6951a61799d6f19944931f189a1c7971
William Hleucka <william.hleucka@gmail.com>
2024-07-09 16:04:55 +0000

    fix: navbar button colour

--- templates/layout/navbar.php
+++ templates/layout/navbar.php
@@ -1,11 +1,11 @@
 <nav id="navbar" class="navbar navbar-light text-dark sticky-top">
-<span class="navbar-brand ps-2 m-0"><?=$app_name?></span>
-<section id="request-progress" class="ps-2 htmx-indicator">
-  <div class="spinner-border spinner-border-sm text-info" role="status">
-    <span class="visually-hidden">Loading...</span>
-  </div>
-</section>
-  <div class="ms-auto pe-2">
-    <span id="toggle-sidebar" class="d-block d-sm-none" data-feather="menu"></span>
-  </div>
+    <span class="navbar-brand ps-2 m-0"><?= $app_name ?></span>
+    <span id="request-progress" class="ps-2 htmx-indicator">
+        <div class="spinner-border spinner-border-sm text-info" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </span>
+    <div class="ms-auto pe-2">
+        <span id="toggle-sidebar" class="d-block d-sm-none text-light" data-feather="menu"></span>
+    </div>
 </nav>
