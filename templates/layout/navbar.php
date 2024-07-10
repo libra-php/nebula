7b63a7f962525946991243ac55d6fae296c21874
William Hleucka <william.hleucka@gmail.com>
2024-07-10 01:32:55 +0000

    fix: breadcrumb request progress

--- templates/layout/navbar.php
+++ templates/layout/navbar.php
@@ -1,10 +1,12 @@
 <nav id="navbar" class="navbar navbar-light text-dark sticky-top">
-    <span class="navbar-brand ps-2 m-0"><?= $app_name ?></span>
-    <span id="request-progress" class="htmx-indicator ps-2">
-        <div class="spinner-border spinner-border-sm text-info" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>
-    </span>
+    <div class="d-flex align-items-center">
+        <span class="navbar-brand ps-2 m-0"><?= $app_name ?></span>
+        <section id="request-progress" class="htmx-indicator ps-2">
+            <div class="spinner-border spinner-border-sm text-info" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </section>
+    </div>
     <div class="ms-auto pe-2">
         <span id="toggle-sidebar" class="d-block d-sm-none text-light" data-feather="menu"></span>
     </div>
