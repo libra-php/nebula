15e654d8c86c8cec9b407fa983096d0a5556c92e
William Hleucka <william.hleucka@gmail.com>
2024-07-12 15:38:29 +0000

    feat: update create/user record module

--- app/Modules/Users.php
+++ app/Modules/Users.php
@@ -4,6 +4,7 @@ namespace App\Modules;
 
 use App\Models\User;
 use Nebula\Framework\Admin\Module;
+use Nebula\Framework\Auth\Auth;
 
 class Users extends Module
 {
@@ -12,7 +13,6 @@ class Users extends Module
     public function init(): void
     {
         $this->user = user();
-        $this->create = $this->delete = $this->edit = false;
         $this->table_columns = [
             "ID" => "id",
             "UUID" => "uuid",
@@ -29,5 +29,54 @@ class Users extends Module
             "Me" => "id = {$this->user->id}",
             "Others" => "id != {$this->user->id}",
         ];
+        $this->form_columns = [
+            "Name" => "name",
+            "E-mail" => "email",
+            "Password" => "password",
+            "Password (agian)" => "password_match",
+        ];
+        $this->form_controls = [
+            "password" => "password",
+            "password_match" => "password"
+        ];
+        $this->validation_rules = [
+            "password" => ["minlength|8", "symbol"],
+            "password_match" => ["match|password"]
+        ];
+        // New records always require these columns
+        if (!$this->id) {
+            $this->validation_rules["password"][] = "required";
+            $this->validation_rules["password_match"][] = "required";
+        }
+    }
+
+    protected function editValueOverride(object &$row): void
+    {
+        // Always hide this value
+        if ($row->column === "password") $row->value = "";
+    }
+
+    public function processCreate(array $request): mixed
+    {
+        // If there is a non-blank password set, then change it
+        if (trim($request["password"]) !== '' && trim($request["password_match"]) !== '') {
+            // Hash the password
+            $request["password"] = Auth::hashPassword($request["password"]);
+        }
+        return parent::processCreate($request);
+    }
+
+    public function processUpdate(string $id, array $request): mixed
+    {
+        // If there is a non-blank password set, then change it
+        if (trim($request["password"]) !== '' && trim($request["password_match"]) !== '') {
+            // Hash the password
+            $request["password"] = Auth::hashPassword($request["password"]);
+        } else {
+            // Otherwise, we don't need to update the password field
+            unset($request["password"]);
+            unset($request["password_match"]);
+        }
+        return parent::processUpdate($id, $request);
     }
 }
