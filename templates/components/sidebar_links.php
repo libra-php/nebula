ee10ee34d2c06abc6e73a705112a04773ba88e1f
William Hleucka <william.hleucka@gmail.com>
2024-07-17 22:35:30 +0000

    fix: avoid hanging hx-indicator

--- templates/components/sidebar_links.php
+++ templates/components/sidebar_links.php
@@ -1,16 +1,19 @@
-<?php
-    foreach ($links as $child): ?>
-        <li class="sidebar-link"><a href="<?=str_replace(".php", '', $child['link'])?>"
-                <?php if ($child['label'] === "Sign Out"): ?>hx-boost=false<?php endif ?>
-                hx-indicator="#request-progress"
-                title="<?=$child['label']?>"
-                onClick="hideSidebar(); htmx.trigger('.sidebar-link', 'htmx:xhr:abort');"
-                data-title="<?=$child['label']?>"
-                data-parent="<?=$parent_link?>"
-                style="padding-left: <?=$depth?>px;"
-                class="link link-dark rounded truncate">
-                <?=$child['label']?>
-            </a></li>
+<?php foreach ($links as $child): ?>
+    <li class="sidebar-link"
+        <?php if ($child['label'] === "Sign Out"): ?>hx-boost=false<?php endif ?>
+        hx-indicator="#request-progress"
+        hx-sync="this:abort"
+        hx-trigger="click queue:all">
+        <a href="<?=str_replace(".php", '', $child['link'])?>"
+            title="<?=$child['label']?>"
+            onClick="hideSidebar();"
+            data-title="<?=$child['label']?>"
+            data-parent="<?=$parent_link?>"
+            style="padding-left: <?=$depth?>px;"
+            class="link link-dark rounded truncate">
+            <?=$child['label']?>
+        </a>
+    </li>
         <?php if (!empty($child['children'])) { echo template("components/sidebar_links.php", ["links" => $child['children'], "parent_link" => $parent_link, "depth" => $depth + 10]); } ?>
     <?php endforeach;
 
--- templates/layout/breadcrumbs.php
+++ templates/layout/breadcrumbs.php
@@ -1,12 +1,17 @@
-<nav id="breadcrumbs" aria-label="breadcrumb" class="mt-2 container-fluid" hx-boost="true" hx-target="main" hx-select="main" hx-swap="outerHTML show:no-scroll" hx-indicator="#request-progress" >
+<nav id="breadcrumbs" aria-label="breadcrumb" class="mt-2 container-fluid" hx-boost="true" hx-target="main" hx-select="main" hx-sync="this:abort" hx-swap="outerHTML show:no-scroll" hx-indicator="#request-progress" >
   <ol class="breadcrumb">
 	<?php foreach ($breadcrumbs as $i => $breadcrumb): ?>
-		<?php if ($breadcrumb->path): ?><a onClick="htmx.trigger('.breadcrumb-link', 'htmx:xhr:abort');" class="breadcrumb-link" href='/admin/<?=$breadcrumb->path?>'><?php endif ?>
+		<?php if ($breadcrumb->path): ?><a class="breadcrumb-link" href='/admin/<?=$breadcrumb->path?>'><?php endif ?>
 		<li class="breadcrumb-item <?php if ($i === count($breadcrumbs) - 1): ?>active<?php endif ?>" <?php if ($i === count($breadcrumbs) - 1): ?>aria-current="page"<?php endif ?>><?=$breadcrumb->title?></li>
 		<?php if ($breadcrumb->path): ?></a><?php endif ?>
 		<?php if ($i !== count($breadcrumbs) - 1): ?>
 		<span class="px-1">></span>
 		<?php endif ?>
 	<?php endforeach ?>
+	  <li id="request-progress" class="breadcrum-item htmx-indicator ps-2">
+		<div class="spinner-border spinner-border-sm text-dark" role="status">
+			<span class="visually-hidden">Loading...</span>
+		</div>
+	  </li>
   </ol>
 </nav>
