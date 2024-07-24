89d4814309ecb7b535d886e385cfa9a5ac5d127b
William Hleucka <william.hleucka@gmail.com>
2024-07-24 20:54:11 +0000

    update: sign in and register routes

--- templates/auth/form/register.php
+++ templates/auth/form/register.php
@@ -25,6 +25,12 @@
 		<p><a hx-boost="true" href="/sign-in" hx-select="main" hx-target="main">Already have an account?</a></p>
 	</div>
 	<div>
-		<button class="btn btn-primary" type="submit">Submit</button>
+		<button class="btn btn-success w-100" type="submit">Create</button>
 	</div>
+	<div class="mt-2 d-flex align-items-center">
+		<img class="rounded me-1" src="/img/nebula.jpeg" height="16px" width="16px" alt="nebula" />
+		<img class="rounded me-1" src="/img/lock.png" height="16px" width="16px" alt="nebula" />
+		<span class="me-1">256-Bit SSL Encryption (AES-256)</span>
+	</div>
+	<div class="text-center"><small>&copy; <?=date("Y")?> All rights reserved</small></div>
 </form>
--- templates/auth/form/sign-in.php
+++ templates/auth/form/sign-in.php
@@ -18,6 +18,12 @@
 		<p><a hx-boost="true" href="/register" hx-select="main" hx-target="main">Don't have an account?</a></p>
 	</div>
 	<div>
-		<button class="btn btn-primary">Submit</button>
+		<button class="btn btn-primary w-100">Enter</button>
 	</div>
+	<div class="mt-2 d-flex align-items-center">
+		<img class="rounded me-1" src="/img/nebula.jpeg" height="16px" width="16px" alt="nebula" />
+		<img class="rounded me-1" src="/img/lock.png" height="16px" width="16px" alt="nebula" />
+		<span class="me-1">256-Bit SSL Encryption (AES-256)</span>
+	</div>
+	<div class="text-center"><small>&copy; <?=date("Y")?> All rights reserved</small></div>
 </form>
